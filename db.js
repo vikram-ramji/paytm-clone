84eb1cc29ae044fc6adacf3ec49c6cb1c5ec538b
Vikram Ramji Iyer <vikramramji24@gmail.com>
2024-08-13 21:55:56 +0530

    Complete backend

---
+++ db.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose")
+const argon2 = require("argon2")
+
+mongoose.connect("mongodb+srv://root:j4XEhIdLlveIYxXD@cluster0.ekkkv5i.mongodb.net/paytm")
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 30
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 30
+    }
+})
+
+userSchema.methods.createHash = async (plainTextPassword) => {
+    return await argon2.hash(plainTextPassword);
+}
+
+userSchema.methods.validatePassword = async (candidatePassword) => {
+    return await argon2.verify(this.password_hash, candidatePassword)
+}
+
+const accountSchema = mongoose.Schema({
+    userId : {
+        type: mongoose.Schema.Types.ObjectId, //Reference to User model
+        ref: "User",
+        required: true
+    },
+    balance : {
+        type: Number,
+        required: true
+    }
+})
+
+const User = mongoose.model("User", userSchema)
+const Account = mongoose.model("Account", accountSchema)
+
+module.exports = {
+    User,
+    Account
+}
